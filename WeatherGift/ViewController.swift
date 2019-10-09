e6bd244cfc6fe928402749d2069e8f82cc62341c
Morgan Glover <glovermo@bc.edu>
2019-10-09 16:54:38 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Morgan Glover on 10/9/19.
+//  Copyright © 2019 Morgan Glover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
