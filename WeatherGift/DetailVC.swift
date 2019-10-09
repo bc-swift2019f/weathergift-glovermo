f806b390d73707f5bb8f7dd0e9c0515517064636
Morgan Glover <glovermo@bc.edu>
2019-10-09 17:39:15 -0400

    7.2 Completed - Set Up UI

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Morgan Glover on 10/9/19.
+//  Copyright © 2019 Morgan Glover. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
---
+++ WeatherGift/PageVC.swift
@@ -0,0 +1,21 @@
+//
+//  PageVC.swift
+//  WeatherGift
+//
+//  Created by Morgan Glover on 10/9/19.
+//  Copyright © 2019 Morgan Glover. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
